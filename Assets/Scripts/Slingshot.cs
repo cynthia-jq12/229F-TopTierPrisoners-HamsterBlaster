1b1ea2cb9cac733782162851a19950d20f3fda63
Gaxoleana <pralit750@gmail.com>
2026-03-28 05:34:54 +0700

    Update on score system

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,39 +4,54 @@ using TMPro;
 public class Slingshot : MonoBehaviour
 {
     private Rigidbody rb;
+    private LineRenderer line;
     public TextMeshProUGUI forceUI;
 
-    public float chargeSpeed = 800f;
-    public float maxForce = 2000f;
+    [Header("Settings")]
+    public float chargeSpeed = 1000f;
+    public float maxForce = 3000f;
     private float currentForce = 0f;
     private bool isCharging = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
     }
 
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        transform.Rotate(0, h * 100f * Time.deltaTime, 0);
+        transform.Rotate(0, h * 150f * Time.deltaTime, 0);
 
         if (Input.GetKey(KeyCode.Space))
         {
             isCharging = true;
+            line.enabled = true;
             currentForce += chargeSpeed * Time.deltaTime;
             currentForce = Mathf.Clamp(currentForce, 0, maxForce);
+
+            DrawAimLine();
         }
 
         if (Input.GetKeyUp(KeyCode.Space) && isCharging)
         {
+            line.enabled = false;
             Shoot();
         }
 
         if (forceUI != null)
         {
-            forceUI.text = "Charging Force: " + currentForce.ToString("F0") + " N";
+            forceUI.text = "Force: " + currentForce.ToString("F0") + " N";
         }
+    }
+
+    void DrawAimLine()
+    {
+        line.SetPosition(0, transform.position);
+        Vector3 endPoint = transform.position + (transform.forward * (currentForce / 100f));
+        line.SetPosition(1, endPoint);
     }
 
     void Shoot()
