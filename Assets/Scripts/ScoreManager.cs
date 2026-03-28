be93fac18400892119dfc23db55266f29632b140
Gaxoleana <pralit750@gmail.com>
2026-03-28 09:40:14 +0700

    Update menu system

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,7 +8,6 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public TextMeshProUGUI scoreText;
 
-    [Header("Win System")]
     public GameObject winPanel;
     public string mainMenuName = "MainMenu";
     public int levelID;
